e99426734668ff01814cb453e74801c7ed91d2e5
ishikajais27 <ishikajais09876@gmail.com>
2025-06-01 20:16:36 +0530

    Fix api

--- QuotesGenerator/script.js
+++ QuotesGenerator/script.js
@@ -6,20 +6,29 @@ function generateQuotes(value) {
 
 async function fetchQuotes() {
   try {
-    const response = await fetch('https://zenquotes.io/api/random ')
+    const response = await fetch('https://api.api-ninjas.com/v1/quotes', {
+      headers: {
+        'X-Api-Key': 'HrmtoltcdFhIf+hu6YXzVA==vsCRgviK8a31z4qu', // Replace with your actual API key
+      },
+    })
 
     if (!response.ok) {
-      throw new Error('Failed to fetch users')
+      throw new Error(`Failed to fetch quotes: ${response.status}`)
     }
 
     const data = await response.json()
 
-    generateQuotes(data.content)
-    console.log(data.content)
+    if (data.length > 0) {
+      generateQuotes(data[0].quote)
+      console.log(data[0].quote)
+    } else {
+      throw new Error('No quotes found')
+    }
   } catch (error) {
     console.error('Error:', error.message)
     document.getElementsByClassName('quote')[0].textContent =
       'Failed to fetch quote. Please try again.'
   }
 }
+
 btn.addEventListener('click', fetchQuotes)
